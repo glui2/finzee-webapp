7cf7486ad451ccb13fb09bbee627864218f77af4
Gavin <gavin.lui@gmail.com>
2020-12-19 23:42:55 +1100

    fixed navbar issue

--- finzee-webapp/src/App.js
+++ finzee-webapp/src/App.js
@@ -1,9 +1,10 @@
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Drawer from "@material-ui/core/Drawer";
-// import Typography from "@material-ui/core/Typography";
-// import Grid from "@material-ui/core/Grid";
+// import { Route, Link } from "react-router-dom";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import Drawer from "@material-ui/core/Drawer";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 import { TransactionContextProvider } from "./contexts/TransactionContext.js";
-// import { Button } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import { TransactionsPage } from "./pages/TransactionsPage.js";
 import "./App.css";
 import React from "react";
@@ -11,11 +12,8 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <TransactionContextProvider>
-        <TransactionsPage></TransactionsPage>
-      </TransactionContextProvider>
-      {/* <Router> */}
-      {/* <Drawer anchor="left" variant="permanent">
+      <Router>
+        <Drawer anchor="left" variant="permanent">
           <Grid container direction="column">
             <Grid item>
               <Button color="primary" href="/transactions">
@@ -38,16 +36,15 @@ function App() {
               </Button>
             </Grid>
           </Grid>
-        </Drawer> */}
-
-      {/* <Route
-          exact
-          path="/"
-          render={() => {
-            <h1>hello world</h1>;
-          }}
-        ></Route>
-      </Router> */}
+        </Drawer>
+        <Switch>
+          <Route exact path="/transactions">
+            <TransactionContextProvider>
+              <TransactionsPage></TransactionsPage>
+            </TransactionContextProvider>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
