cb716d438c11f9adf925a413235e97ceb00835a1
Gavin <gavin.lui@gmail.com>
2020-12-20 00:59:08 +1100

    add profile pic icon to cards

--- finzee-webapp/src/components/PersonCard.js
+++ finzee-webapp/src/components/PersonCard.js
@@ -1,8 +1,22 @@
 import { Button, Grid, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import AccountCircleOutlinedIcon from "@material-ui/icons/AccountCircleOutlined";
 import React from "react";
 import Box from "@material-ui/core/Box";
 
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+    borderRadius: "5em",
+    width: "80%",
+  },
+  profilePicture: {
+    fontSize: "50px",
+  },
+}));
+
 const PersonCard = (props) => {
+  const classes = useStyles();
   var name = props.name;
   var job = props.job;
   var experience = props.experience;
@@ -10,8 +24,23 @@ const PersonCard = (props) => {
   var certificate = props.certificate;
 
   return (
-    <Box borderColor="primary.main" border={1} borderRadius={20} m={1} p={3}>
+    <Box
+      borderColor="primary.main"
+      width="250px"
+      height="250px"
+      border={1}
+      borderRadius={20}
+      m={1}
+      p={3}
+    >
       <Grid container direction="column">
+        <Grid item>
+          <AccountCircleOutlinedIcon
+            color="primary"
+            fontSize="large"
+            className={classes.profilePicture}
+          ></AccountCircleOutlinedIcon>
+        </Grid>
         <Grid item>
           <Typography color="primary">
             <strong>{name}</strong>
@@ -30,7 +59,7 @@ const PersonCard = (props) => {
           <Typography>{certificate}</Typography>
         </Grid>
         <Grid item>
-          <Button variant="outlined" color="primary" className="hire-button">
+          <Button variant="outlined" color="primary" className={classes.button}>
             Hire
           </Button>
         </Grid>
