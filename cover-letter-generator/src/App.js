2458fa62f89f22be7fae9320111a47b4f1609239
Gavin <gavin.lui@gmail.com>
2021-02-04 22:33:21 +1100

    fixed styling

--- cover-letter-generator/src/App.js
+++ cover-letter-generator/src/App.js
@@ -5,70 +5,115 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Cover Letter Generator</h1>
+        <h3 className="heading">Cover Letter Generator</h3>
       </header>
       <section>
         <Form>
           <Form.Group as={Row} controlId="formCompanyName">
-            <Form.Label className="form-label">Company Name:</Form.Label>
-            <Form.Control
-              type=""
-              placeholder="Enter the company name"
-              className="form-input"
-            />
+            <Form.Label column sm={2} className="form-label">
+              Company Name:
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control
+                type=""
+                placeholder="Enter the company name"
+                className="form-input"
+              />
+            </Col>
           </Form.Group>
           <Form.Group as={Row} controlId="formRoleName">
             <Form.Label className="form-label" column sm="2">
               Role Name:
             </Form.Label>
-            <Form.Control
-              placeholder="Enter the role you are applying for"
-              className="form-input"
-            />
+            <Col sm={10}>
+              <Form.Control
+                placeholder="Enter the role you are applying for"
+                className="form-input"
+              />
+            </Col>
           </Form.Group>
-          <Form.Group controlId="formSkillsList">
-            <Form.Label className="form-label">Skills to highlight</Form.Label>
-            <Row>
-              {[
-                "HTML/CSS/JS",
-                "React",
-                "Vue",
-                "Python",
-                "Flask",
-                "Django",
-                "Java",
-                "OOP",
-                "SQL",
-                "CI/CD",
-              ].map((skill) => (
-                <div key={`skill-${skill}`} className="mb-3">
-                  <Form.Check
-                    inline
-                    type="checkbox"
-                    id={`skill-${skill}`}
-                    label={skill}
-                  ></Form.Check>
-                </div>
-              ))}
-            </Row>
+          <Form.Group as={Row} controlId="formFrontendSkillsList">
+            <Form.Label className="form-label" column sm="3">
+              Front end skills:
+            </Form.Label>
+            <Col sm="9">
+              <Row>
+                {["HTML/CSS/JS", "React", "Vue", "Kotlin", "Wordpress"].map(
+                  (skill) => (
+                    <div key={`frontend-skill-${skill}`} className="mb-2">
+                      <Form.Check
+                        inline
+                        type="checkbox"
+                        id={`frontend-skill-${skill}`}
+                        label={skill}
+                      ></Form.Check>
+                    </div>
+                  )
+                )}
+              </Row>
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} controlId="formBackendSkillsList">
+            <Form.Label className="form-label" column sm="3">
+              Back end skills:
+            </Form.Label>
+            <Col sm="9">
+              <Row>
+                {["Python", "Flask", "Django", "Java", "OOP", "SQL"].map(
+                  (skill) => (
+                    <div key={`backend-skill-${skill}`} className="mb-2">
+                      <Form.Check
+                        inline
+                        type="checkbox"
+                        id={`backend-skill-${skill}`}
+                        label={skill}
+                      ></Form.Check>
+                    </div>
+                  )
+                )}
+              </Row>
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} controlId="formToolsList">
+            <Form.Label className="form-label" column sm="3">
+              Software tools:
+            </Form.Label>
+            <Col sm="9">
+              <Row>
+                {["Git", "CI/CD", "Unit testing", "AWS", "GCP"].map((skill) => (
+                  <div key={`software-tool-${skill}`} className="mb-2">
+                    <Form.Check
+                      inline
+                      type="checkbox"
+                      id={`software-tool-${skill}`}
+                      label={skill}
+                    ></Form.Check>
+                  </div>
+                ))}
+              </Row>
+            </Col>
           </Form.Group>
           <Form.Group as={Row} controlId="formOtherSkills">
             <Form.Label className="form-label" column sm="2">
               Other skills:
             </Form.Label>
-            <Form.Control
-              placeholder="Enter any other skills that aren't listed"
-              className="form-input"
-            />
+            <Col sm={10}>
+              <Form.Control
+                placeholder="Enter any other skills that aren't listed"
+                className="form-input"
+              />
+            </Col>
           </Form.Group>
           <Form.Group as={Row} controlId="formRoleName">
             <Form.Label className="form-label" column sm="2">
               Interests:
             </Form.Label>
-            <Form.Control
-              placeholder="Enter any other interests or projects"
-              className="form-input"
-            />
+            <Col sm={10}>
+              <Form.Control
+                placeholder="Enter any other interests or projects"
+                className="form-input"
+              />
+            </Col>
           </Form.Group>
           <Button variant="primary" type="submit" className="submit-button">
             Submit
